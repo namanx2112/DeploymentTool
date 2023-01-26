7714cdd6b0c17c132ca130774725125bf721cce9
VikramRangaswamy <vikram_rangaswamy@hotmail.com>
2023-01-26 12:53:32 +0530

    API-FrameWork - Checkin

---
+++ AppStart/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DeploymentTool.AppStart
+{
+    public class RouteConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiRoute",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+        }
+    }
+}
