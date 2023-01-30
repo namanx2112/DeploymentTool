351e300f55e156ee03f5c70cb6505c458a066d3e
namanx2112 <namanx@gmail.com>
2023-01-31 00:46:29 +0530

    edfdsf

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DeploymentTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Zytos Solutions")]
 [assembly: AssemblyProduct("DeploymentTool")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
